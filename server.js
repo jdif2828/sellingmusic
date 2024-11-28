f4696ae47de3c82d2466cb5d7d62fb06d3d70e8d
jdif2828 <jeganathanjack2828@gmail.com>
2024-11-28 19:34:06 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+const http= require('http');
+const fs = require('fs');
+const index = fs.readFileSync('index.js');
 
+let app = http.createServer((req, res) => {
+res.writeHead(200,{'Content-Type': 'text/plain'});
+res.end(index);
+});
+
+app.listen(9030, '0.0.0.0');
+console.log('Node server running on port 9030')
